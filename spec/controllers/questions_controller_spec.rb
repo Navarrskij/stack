a5f7de38ff94c0742a777454c55a360d6e812fd9
Navarrskij <rostov-7@yandex.ru>
2016-10-13 00:50:53 +0300

    cont5

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe QuestionsController, type: :controller do
     let(:question) { create(:question) }
     before {get :show, id: question}
     it 'assigns the requested question to @question' do
-      expect(assigns(:question)).to eq question
+      expect(assigns(:question)).to eq @question
     end 
     it 'renders show view' do
       expect(response).to render_template :show
