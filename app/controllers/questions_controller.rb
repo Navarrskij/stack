0c687ec5c6d4592b396b6bc90669cd0dad3d5043
Navarrskij <rostov-7@yandex.ru>
2016-10-13 01:59:45 +0300

    cont7

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,22 @@
 class QuestionsController < ApplicationController
+  before_action :load_qwe, only: [:show, :edit]
   def index
     @questions = Question.all
   end
 
   def show
-    @question = Question.find(params[:id])
   end
 
   def new
     @question = Question.new
+  end
+
+  def edit
+  end
+
+  private
+
+  def load_qwe
+     @question = Question.find(params[:id])
   end
 end
