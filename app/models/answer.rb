e915c12b72dcbf4ae9e15542ead8d3ebc920a9c4
Navarrskij <rostov-7@yandex.ru>
2016-10-30 18:18:42 +0300

    Polymorphic attachments

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -13,7 +13,9 @@
 class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
+  has_many :attachments, as: :attachmentable
   validates :body, presence: true
+  accepts_nested_attributes_for :attachments
 
   def best!
   	transaction do
---
+++ spec/acceptance/add_files_to_answer_spec.rb
@@ -0,0 +1,27 @@
+require_relative 'acceptance_helper'
+ 
+feature 'Add files to answer', %q{
+   In order to illustrate my answer
+   As an answer author
+   I want to be able to attach file
+} do
+
+  given(:user) { create(:user) }
+  given(:question) { create(:question)}
+
+  background do
+    sign_in(user)
+    visit question_path(question)
+  end
+ 
+  scenario "User add file when create answer" do
+
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'text text text'
+    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    click_on 'Create Question'
+    click_on 'Show'
+
+    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+  end
+end 
