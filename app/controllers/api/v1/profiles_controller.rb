a46bda6ed168317ef1feab8144daeda00877a12c
Navarrskij <rostov-7@yandex.ru>
2016-12-07 17:51:57 +0300

    fix spaces

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -12,7 +12,7 @@ class Api::V1::ProfilesController < ApplicationController
 		respond_with User.where.not(id: current_resource_owner.id)
 	end
 
-	protected
+protected
 
 	def current_resource_owner
 		@current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
