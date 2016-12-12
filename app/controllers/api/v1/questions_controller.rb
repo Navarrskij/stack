358f218f643af85fa7566f5a572306d035476013
Navarrskij <rostov-7@yandex.ru>
2016-12-13 00:30:33 +0300

    add api for questions

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -6,4 +6,24 @@ class Api::V1::QuestionsController < Api::V1::BaseController
     @questions = Question.all
     respond_with @questions
   end
+
+  def list_questions
+    @questions = Question.all
+    respond_with @questions
+  end
+
+  def show
+    @question = Question.find(params[:id])
+    respond_with @question, serializer: QuestionAggregateSerializer
+  end
+
+  def create
+    respond_with (@question = Question.create(questions_params.merge(user: current_resource_owner)))
+  end
+
+private
+
+  def questions_params
+    params.require(:question).permit(:title, :body)
+  end
 end
