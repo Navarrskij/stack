868b075bbd39b211eeaced7f5ab6f250838e587b
Navarrskij <rostov-7@yandex.ru>
2017-01-17 01:01:35 +0300

    fix tabs

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -11,7 +11,6 @@
 #
 
 class Attachment < ApplicationRecord
-	belongs_to :attachmentable, polymorphic: true, optional: true, touch: true
-	mount_uploader :file, FileUploader
-
+  belongs_to :attachmentable, polymorphic: true, optional: true, touch: true
+  mount_uploader :file, FileUploader
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -12,7 +12,7 @@
 #
 
 class Comment < ApplicationRecord
-	belongs_to :commentable, polymorphic: true, touch: true
-	belongs_to :user
-	validates :body, presence: true
+  belongs_to :commentable, polymorphic: true, touch: true
+  belongs_to :user
+  validates :body, presence: true
 end
